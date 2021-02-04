64e16fbaa4a3b6d9ab22b9ca01a80eaeedb57512
Berenice Ortiz <ereb_83@hotmail.com>
2021-02-04 12:55:30 -0800

    Fixing get all categories endpoint

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -3,6 +3,6 @@ const ProductController = require("../controllers/product.controller");
 module.exports = function (app) {
     app.get("/api/product", ProductController.getAllProducts);
     app.get("/api/product/:id", ProductController.getProduct);
-    app.get("/api/product/category", ProductController.getAllCategories);
     app.get("/api/product/category/:id", ProductController.getProductsByCategory);
+    app.get("/api/category", ProductController.getAllCategories);
 };
